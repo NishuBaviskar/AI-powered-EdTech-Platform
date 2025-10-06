5ee6d4efd5dd312dd85f9a9138c32d566cee7e2a
NishuBaviskar <158685546+NishuBaviskar@users.noreply.github.com>
2025-10-06 11:45:49 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+/*import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
@@ -23,6 +23,26 @@ api.interceptors.request.use(
   error => {
     return Promise.reject(error);
   }
+);
+
+export default api;*/
+
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || '/api',
+  headers: { 'Content-Type': 'application/json' }
+});
+
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
 );
 
 export default api;
